87facff4757e3681da5dc7481ce5c0d1220080c0
brfyamada <bfyamada@outlook.com>
2023-03-08 19:55:26 -0300

    Fix flyway integration

---
+++ src/main/java/com/brfyamada/webflux/config/FlywayConfig.java
@@ -0,0 +1,28 @@
+package com.brfyamada.webflux.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class FlywayConfig {
+
+    private final Environment env;
+
+    public FlywayConfig(final Environment env) {
+        this.env = env;
+    }
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        return new Flyway(Flyway.configure()
+                .baselineOnMigrate(true)
+                .defaultSchema(env.getRequiredProperty("spring.flyway.schemas"))
+                .dataSource(
+                        env.getRequiredProperty("spring.flyway.url"),
+                        env.getRequiredProperty("spring.flyway.user"),
+                        env.getRequiredProperty("spring.flyway.password"))
+        );
+    }
+}
